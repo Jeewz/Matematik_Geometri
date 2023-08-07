2c24e0e09e7825c0e3c45f012a6cc85178533e6a
Jeewz <86675840+Jeewz@users.noreply.github.com>
2023-08-07 15:43:28 +0300

    Add files via upload

---
+++ daireninalanı.py
@@ -0,0 +1,8 @@
+while(True):
+    pi_sayisi = 3.14
+    print("Pi sayısı otomatik 3.14 alınmıştır.")
+    yari_çap = int(input("Yarı çapı girin:"))
+
+    sonuc = pi_sayisi * (yari_çap*yari_çap)
+    print(sonuc)
+    break
