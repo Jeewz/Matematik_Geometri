2ddddf516bbc940ee498800761267ece00510330
Jeewz <86675840+Jeewz@users.noreply.github.com>
2023-08-07 15:39:03 +0300

    Add files via upload

---
+++ üçgeninalanı.py
@@ -0,0 +1,7 @@
+while(True):
+    taban = int(input("Üçgenin taban sayısı girin:"))
+    yukseklik = int(input("Üçgenin yüksekliğini girin:"))
+
+    sonuc = taban * yukseklik / 2
+    print(sonuc)
+    break
